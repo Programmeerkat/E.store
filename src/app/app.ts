9fcfddd82c7f78ed81760772bc41ef891cef74a1
Programmeerkat <96639872+Programmeerkat@users.noreply.github.com>
2026-02-13 15:03:49 +0100

    removed banner and its service

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,33 +2,24 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { Card } from './components/card/card';
-import { Banner } from './components/banner/banner';
 import { Footer } from './components/footer/footer';
 import { Header } from './components/header/header';
 
-import { BannerService } from './services/banner-service';
 import { ProductService } from './services/product-service';
 
 import { Product } from "./types";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Banner, Card, Footer, Header],
+  imports: [RouterOutlet, Card, Footer, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit {
-  private bannerService = inject(BannerService);
-  banner = signal<Product[]>([]);
-  
   private productService = inject(ProductService);
   products = signal<Product[]>([]);
 
-  ngOnInit() {
-    this.bannerService.getBanner().subscribe((data: any) => {
-      this.banner.set(data.banner);
-    });
-    
+  ngOnInit() { 
     this.productService.getProducts().subscribe((data: any) => {
       this.products.set(data.products);
     });
