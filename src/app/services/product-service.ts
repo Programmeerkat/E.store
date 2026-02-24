b6b576593dc46c9f74b58de323b838cd906bf8e6
Programmeerkat <96639872+Programmeerkat@users.noreply.github.com>
2026-02-24 09:36:07 +0100

    added env variables

--- src/app/services/product-service.ts
+++ src/app/services/product-service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Product } from '../types';
 
+import {environment} from './../../environments/environment';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -9,10 +11,10 @@ export class ProductService {
   constructor(private http: HttpClient) { }
 
   getProducts() {
-    return this.http.get<Product[]>('https://dummyjson.com/products');
+    return this.http.get<Product[]>(`${environment.apiUrl}/products`);
   }
 
   getSingleProduct(id: number) {
-    return this.http.get<Product[]>(`https://dummyjson.com/products/${id}`);
+    return this.http.get<Product[]>(`${environment.apiUrl}/products/${id}`);
   }
 }
